062e6c59aaf3727a349e712a736b0c9a51367833
Shivam Gaur <shivam.gaur@lendingkart.com>
2020-01-31 15:28:20 +0530

    Initial Commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase  : {
+    apiKey: "AIzaSyDPOeCE6eDVH6RD3Vo2Y7gC1NsdwX6HFD8",
+    authDomain: "todolistapp-85fda.firebaseapp.com",
+    databaseURL: "https://todolistapp-85fda.firebaseio.com",
+    projectId: "todolistapp-85fda",
+    storageBucket: "todolistapp-85fda.appspot.com",
+    messagingSenderId: "899725052884",
+    appId: "1:899725052884:web:6a03fc893a4d87daae2441",
+    measurementId: "G-VZ4K2V5NM0"
+  }
 };
 
 /*
